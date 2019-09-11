991da70d6b9a5b1030431febcfb2a1b80bb40d66
Mownika V <mownika.v@msil.ad>
2019-09-11 14:59:26 +0530

    update

--- SVersion1/src/com/example/Sversion1.java
+++ SVersion1/src/com/example/Sversion1.java
@@ -5,6 +5,10 @@ public class Sversion1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Sversion is working fine");
+		
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
 	}
 
 }
