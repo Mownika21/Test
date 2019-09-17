58abe0e709491244e1271cd6b3c1dba1add0fced
Mownika V <mownika.v@msil.ad>
2019-09-17 12:08:28 +0530

    Sversion 2 changes

--- SVersion1/src/com/example/Sversion2.java
+++ SVersion1/src/com/example/Sversion2.java
@@ -1,5 +1,14 @@
 package com.example;
 
 public class Sversion2 {
+	
+	
+	public static void getData() {
+		System.out.println("response");
+		
+	}
+	public static void main(String[] args) {
+		getData();
+	}
 
 }
